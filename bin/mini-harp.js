e6179c5d7fd48f2f601ec9eda299229c10524d6f
he.redemption <he.redemption@gmail.com>
2015-01-05 20:35:55 +0800

    serving static content

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -3,13 +3,21 @@
 var parseArgs = require('minimist');
 var createMiniHarp = require('mini-harp');
 
-var port = 4000;
 var args = parseArgs(process.argv);
-var app = createMiniHarp();
 
+var port = 4000;
 if(args["port"]){
 	port = args["port"];
 }
 
+var root = process.cwd();
+if(args._.length > 2){
+	root = args._[2];
+}
+
+var app = createMiniHarp(root);
+
 console.log("Starting mini-harp on http://localhost:%d", port);
+console.log(args._);
+console.log(root);
 app.listen(port);
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 module.exports = createMiniHarp;
 
-function createMiniHarp(){
+function createMiniHarp(root){
 	var connect = require('connect');
+	var serveStatic = require('serve-static');
+
+
 	var app = connect();
 	var currentTime = require('currenttime')
 
-	app.use(currentTime);
+	app
+	   .use(currentTime)
+	   .use(serveStatic(root));
 
 	return app;
 }
