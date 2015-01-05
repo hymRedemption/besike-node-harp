cef1236b5015c421358b998fb8bfac68fe87c7a2
he.redemption <he.redemption@gmail.com>
2015-01-05 20:09:38 +0800

    Implement a current time middleware

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ module.exports = createMiniHarp;
 function createMiniHarp(){
 	var connect = require('connect');
 	var app = connect();
+	var currentTime = require('currenttime')
+
+	app.use(currentTime);
+
 	return app;
 }
